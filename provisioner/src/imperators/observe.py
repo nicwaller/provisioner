150867d0872527f1bd77738b407433f455254377
Nic Waller <code@nicwaller.com>
2021-05-23 00:38:23 -0700

    restore compat with Ubuntu 18.04
    and Python 3.6.9

--- provisioner/src/imperators/observe.py
+++ provisioner/src/imperators/observe.py
@@ -20,16 +20,19 @@ class Observe(BaseImperator):
     def apply(self):
         if self.key in Observe.touched_resources:
             logger.info(f"Triggered observer {self.key}")
-            out = subprocess.run(["bash", "-c", self.command], capture_output=True)
+            # FIXME: capture output the hard way, because capture_output is available in 3.7 but we have 3.6.9 :(
+            out = subprocess.run(["bash", "-c", self.command])
+            # out = subprocess.run(["bash", "-c", self.command], capture_output=True)
             if out.returncode != 0:
                 logger.error(
                     f"Something went wrong while running this observer {self.key}"
                 )
-                print(out.stdout)
-                print(out.stderr, file=sys.stderr)
+                # print(out.stdout)
+                # print(out.stderr, file=sys.stderr)
                 # Being able to roll back a notifier resource would be interesting, but too much work
             else:
-                logger.info(out.stdout.decode("utf-8").strip())
+                # logger.info(out.stdout.decode("utf-8").strip())
+                pass
         elif self.key in Observe.untouched_resources:
             logger.debug(f"Skipping observer for {self.key}")
         else:
