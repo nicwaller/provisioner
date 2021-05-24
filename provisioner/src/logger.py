284ff06cd930febbf5aeeff3696e6c60f6862fc2
Nic Waller <code@nicwaller.com>
2021-05-23 18:06:18 -0700

    add JSON structured logging

--- provisioner/src/logger.py
+++ provisioner/src/logger.py
@@ -1,20 +1,41 @@
+from datetime import datetime
 import logging
+import os
 import sys
 
 # FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
 # logging.basicConfig(format=FORMAT)
 # d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
+from pythonjsonlogger import jsonlogger
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        log_record['level'] = record.levelname
+
 
 rootLogger = logging.getLogger()
 rootLogger.setLevel(logging.INFO)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
-handler.setFormatter(formatter)
-rootLogger.addHandler(handler)
+print(os.getenv('LOG', 'plain'))
+if os.getenv('LOG', 'plain') == 'JSON':
+    jsonHandler = logging.StreamHandler()
+    jsonHandler.setFormatter(CustomJsonFormatter('%(timestamp)s %(level)s %(name)s %(message)s'))
+    rootLogger.addHandler(jsonHandler)
+else:
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+    rootLogger.addHandler(handler)
+
 
 logger = rootLogger.getChild("provisioner")
 logger.debug("Logger is ready")
 
 # logger.warning('Protocol problem: %s', 'connection reset', extra=d)
+
